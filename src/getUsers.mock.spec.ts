ccd1802f3253be176bc9e385e1cea0708b8b93e0
ericnkaka <nkakaeric96@gmail.com>
2023-12-28 16:28:33 +0200

    feat: done

--- src/getUsers.mock.spec.ts
+++ src/getUsers.mock.spec.ts
@@ -1,31 +1,19 @@
-import { getUser } from './getUsers';
-import {findUser} from './user'
+import { getUser } from "./getUsers";
+import { findUser } from "./user";
 
-jest.mock('./user')
+jest.mock("./user");
 
-// jest.mock('library/db', () => {
-//   const actual = jest.requireActual('library/db');
-//   return {
-//       ...actual,
-//       isActive: jest.fn(),
-//   };
-// });
-
-describe("When testing getUser with jest mock", ()=>{
-  const findUserMock = findUser as jest.Mock
-
-
-  it("should return active user", async()=>{
-      findUserMock.mockResolvedValue({
-        id: 10,
-        username: 'mocked username',
-        active: true
-      })
-
-      const user = await getUser(10);
-
-      expect(findUserMock).toBeCalledTimes(1);
-      expect(user.id).toBe(10);
-      expect(user.active).toBeTruthy();
-  })
-})
+describe("It should test getUser function", () => {
+  const findUserMock = findUser as jest.Mock;
+  it("test getUser with different IDs", async () => {
+    findUserMock.mockResolvedValue({
+      id: 10,
+      username: "mocked username",
+      active: true,
+    });
+    const user = await getUser(10);
+    expect(user.id).toBe(10);
+    expect(findUserMock).toHaveBeenCalledTimes(1);
+    expect(user.active).toBeTruthy();
+  });
+});
--- src/user.ts
+++ src/user.ts
@@ -1,18 +1,18 @@
 export type User = {
-  id: number
-  username: string
-  active: boolean
-}
+  id: number;
+  username: string;
+  active: boolean;
+};
 
-const isActive = (userId: number):boolean => userId % 2 === 0;
+const isActive = (userId: number): boolean => userId % 2 === 0;
 
-export const findUser = async (id: number): Promise<User> =>{
-  console.log('We are in findUser() line 10');
-  await new Promise(resolve => setTimeout(resolve, 1500));
+export const findUser = async (id: number): Promise<User> => {
+  console.log("We are in findUser() line 10");
+  await new Promise((resolve) => setTimeout(resolve, 1500));
 
   return {
     id,
     username: `username ${id}`,
-    active: isActive(id)
-  }
-}
+    active: isActive(id),
+  };
+};
